523008f1bb5ba6602dbbbd672679f28dbab78504
bijoymamud <bijoymamud.09@gmail.com>
2024-08-21 12:04:37 +0600

    middlewere

--- src/Pages/redux/store.jsx
+++ src/Pages/redux/store.jsx
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from './features/counter/counterSlice'
+import counterReducer from './features/counter/counterSlice';
+import looger from '../redux/middleweres/logger';
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(looger),
 })
 
 export default store;
